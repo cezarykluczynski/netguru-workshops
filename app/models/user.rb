fb7619adaeff3582451038de42838920c4e1c5df
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-03-03 20:38:10 +0100

    Issue #1: missing fields added to models and schema via migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :firstname, :lastname, presence: true
+
   has_many :reviews
   has_many :products
+
+  def admin?
+    return @admin
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606091435) do
+ActiveRecord::Schema.define(version: 20150303190625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20140606091435) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
+    t.string   "firstname",              default: "", null: false
+    t.string   "lastname",               default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -54,6 +56,7 @@ ActiveRecord::Schema.define(version: 20140606091435) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
