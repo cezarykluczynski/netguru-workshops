a304915f38f1ad67c1a97e61e3324596b63c6a2d
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-03-03 22:02:59 +0100

    Issue #4: product model fixed with more price validation and avarage price calculation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,19 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :price, :description, :title, presence: true
+  validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
+
+  def average_rating
+    rating = 0
+    reviews_count = 0
+
+    self.reviews.each { |review|
+      reviews_count += 1
+      rating += review.rating
+    }
+
+    reviews_count ? rating.fdiv( reviews_count ) : 0
+  end
 end
