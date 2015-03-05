6315f6a566975666e72640a312abd79600f78c0c
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-03-05 19:05:36 +0100

    Issue #11: navbar links added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,10 +7,8 @@ class CategoriesController < ApplicationController
   expose(:product) { Product.new }
 
   def redirect_non_admins_to_login
-    logger.debug "action_name"
-    logger.debug action_name
-    if action_name != "index" and action_name != "show" and not current_user.admin?
-      redirect_to  :controller => "devise/sessions", :action => :new
+    if action_name != "index" and action_name != "show" and not current_user.admin
+      redirect_to :controller => "devise/sessions", :action => :new
     end
   end
 
